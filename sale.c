4f7aad50a20503969c1a97d3d4c21b4dfc4146ef
6604062610322Danich <s6604062610322@email.kmutnb.ac.th>
2025-12-09 14:08:18 +0700

    commit2

---
+++ sale.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	float a,a1,b,b1,c,c1;
+	scanf("%f",&a);
+	a1 = (a*95)/100;
+	printf("%.2f\n",a1);
+	scanf("%f",&b);
+	b1 = ((a+b)*85)/100;
+	printf("%.2f\n",b1);
+	scanf("%f",&c);
+	c1 = ((a+b+c)*70)/100;
+	printf("%.2f\n",c1);
+	
+	return 0;
+}
